de3a4e8dc2f42aec9e1cd4bbe3a1229cffa1c994
Parth Mahendra <ParthMahendra@outlook.com>
2019-06-28 21:23:02 +0100

    Forgot to save file before commiting

--- save.py
+++ save.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 def declareArray(m, n, integer):
     arr = []
     char = 0 if integer else ""
@@ -7,15 +10,36 @@ def declareArray(m, n, integer):
             arr[y].append(char)
     return arr
 
+
 def binary(i):
     n = 0
     binarr = []
     while 2 ** n <= i:
         n += 1
-    for x in range(n-1,-1,-1):
+    for x in range(n - 1, -1, -1):
         if (2 ** x) <= i:
             i -= (2 ** x)
             binarr.append("1")
         else:
             binarr.append("0")
     return "".join(binarr)
+
+
+def loadImage(location):
+    img = Image.open(location)
+    x, y = img.size
+    return img, x, y
+
+
+def main():
+    # str = input("Enter String To Be Hidden: ")
+    str = "bruh"
+    # location = input("Enter file location: ")
+    location = "sample.png"
+    img, height, size = loadImage(location)
+    arr = []
+    for c in str:
+        arr.append(binary(ord(c)))
+
+
+main()
