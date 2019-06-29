0d57e01132d9ef3cbb3cac3cc14b0f59023c2b7e
Parth Mahendra <ParthMahendra@outlook.com>
2019-06-29 20:54:50 +0100

    Migrating functions from generation file to extraction file

--- extract.py
+++ extract.py
@@ -1,8 +1,20 @@
-def binary(i, t=0):
-    f = int(str(i)[:1])
-    if f == 1:
-        t += (2 ** len(i))
-    if len(i) != 1:
-        binary(int(str(i)[1:]))
-    else:
-        return t += 
+def binary(i):
+    n = 0
+    binarr = []
+    while 2 ** n <= i:
+        n += 1
+    for x in range(n - 1, -1, -1):
+        if (2 ** x) <= i:
+            i -= (2 ** x)
+            binarr.append("1")
+        else:
+            binarr.append("0")
+    return "".join(binarr)
+
+
+def denary(string):
+    string = string.strip("0")
+    t = 0
+    for b in range(len(string) - 1, -1, -1):
+        t += int(string[b]) * (2 ** b)
+    return t
